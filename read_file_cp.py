0eafd5e394153f79950a5fdecb765e48b9bfd5ef
guptaNswati <sgupta1505@gmail.com>
2017-02-26 01:02:49 +0000

    upadted read_file to add common package count

--- read_file_cp.py
+++ read_file_cp.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import re
+from collections import OrderedDict
 """
 Read dockerfile created by dummper and tokenize it.
 """
 
-def read_lines(filename=""):
-    install_dic = {}
+def read_lines(filename="", install_dic = {}):
     srch_str = "install"
     with open(filename, 'r') as f:
         lines = f.readlines()
@@ -20,9 +20,16 @@ def read_lines(filename=""):
                         for sub_words in word:
                             if sub_words is not None:
                                 install_dic[sub_words] = install_dic.get(sub_words, 0) + 1
+                                install_dic = OrderedDict(install_dic)
 
+        
         for key in install_dic:
             print(key, install_dic[key])
+        return (install_dic)
+        
 
 if __name__ == "__main__":
-    read_lines("/tmp/test")
+    install_dic = read_lines("/tmp/test")
+    print(install_dic.items())
+    install_dic = read_lines("test2", install_dic)
+    print(install_dic.items())
